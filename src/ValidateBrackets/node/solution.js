8b3b95a12ce416ac35397ddd28893f629dfba184
Bob Jacobs <bob.jacobs@outlook.com>
2024-04-22 17:06:12 -0400

    adding node

--- src/ValidateBrackets/node/solution.js
+++ src/ValidateBrackets/node/solution.js
@@ -1,1 +1,24 @@
-module.exports = class Solution { };
+class Solution {
+    isValid(s) {
+        const stack = [];
+        const brackets = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+
+        for (let char of s) {
+            if (brackets[char]) {
+                if (stack.pop() !== brackets[char]) {
+                    return false;
+                }
+            } else {
+                stack.push(char);
+            }
+        }
+        return stack.length === 0;
+    }
+}
+
+module.exports = Solution;
+
--- src/ValidateBrackets/node/test.js
+++ src/ValidateBrackets/node/test.js
@@ -1,2 +1,14 @@
-const solution = require('./solution');
-test('bracket validation', () => { });
+const Solution = require('./solution');
+
+describe('Solution', () => {
+    test('validates the correct bracket sequence', () => {
+        const solution = new Solution();
+        expect(solution.isValid("()")).toBe(true);
+        expect(solution.isValid("()[]{}")).toBe(true);
+        expect(solution.isValid("(]")).toBe(false);
+        expect(solution.isValid("([)]")).toBe(false);
+        expect(solution.isValid("{[]}")).toBe(true);
+        expect(solution.isValid("")).toBe(true);  // Empty string should also return true
+    });
+});
+
