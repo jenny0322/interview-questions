0ef4595301067d0472a94fd4f944715759ca3ad4
Bob Jacobs <bob.jacobs@outlook.com>
2024-04-25 10:53:26 -0400

    update formatting

--- src/TwoSum/csharp/TwoSumLib/Class1.cs
+++ src/TwoSum/csharp/TwoSumLib/Class1.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 
 namespace TwoSumLib 
 {
-    public class Solution {
-        public int[] TwoSum(int[] nums, int target) {
+    public class Solution 
+    {
+        public int[] TwoSum(int[] nums, int target) 
+        {
             Dictionary<int, int> numMap = new Dictionary<int, int>();
 
-            for (int i = 0; i < nums.Length; i++) {
+            for (int i = 0; i < nums.Length; i++) 
+            {
                 int complement = target - nums[i];
-                if (numMap.ContainsKey(complement)) {
+                if (numMap.ContainsKey(complement)) 
+                {
                     return new int[] { numMap[complement], i };
                 }
-                if (!numMap.ContainsKey(nums[i])) {
+                if (!numMap.ContainsKey(nums[i])) 
+                {
                     numMap.Add(nums[i], i);
                 }
             }
