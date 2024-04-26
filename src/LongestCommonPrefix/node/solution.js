7782d1ff54931b4fe0231f5a0ac345f30ef6d8b1
Bob Jacobs <bob.jacobs@outlook.com>
2024-04-26 19:27:05 -0400

    pass

--- src/LongestCommonPrefix/node/solution.js
+++ src/LongestCommonPrefix/node/solution.js
@@ -1,16 +1,15 @@
 class Solution {
     longestCommonPrefix(strs) {
-        return "zzz";
-        // if (!strs.length) return "";
+        if (!strs.length) return "";
         
-        // let prefix = strs[0];
-        // for (let i = 1; i < strs.length; i++) {
-        //     while (strs[i].indexOf(prefix) !== 0) {
-        //         prefix = prefix.substring(0, prefix.length - 1);
-        //         if (prefix === "") return "";
-        //     }
-        // }
-        // return prefix;
+        let prefix = strs[0];
+        for (let i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) !== 0) {
+                prefix = prefix.substring(0, prefix.length - 1);
+                if (prefix === "") return "";
+            }
+        }
+        return prefix;
     }
 }
 
