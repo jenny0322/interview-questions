b7bb3738c1ca8b68dbb09e72eec24ca007c1e89f
Bob Jacobs <bob.jacobs@outlook.com>
2024-04-26 19:39:07 -0400

    pass

--- src/LongestCommonPrefix/python/solution.py
+++ src/LongestCommonPrefix/python/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def longest_common_prefix(self, strs):
-        return "zzz"
-        # if not strs:
-        #     return ""
+        if not strs:
+            return ""
         
-        # # Start by assuming the first string is the common prefix
-        # prefix = strs[0]
-        # for s in strs[1:]:
-        #     # Adjust the prefix until it matches the beginning of each string
-        #     while not s.startswith(prefix):
-        #         prefix = prefix[:-1]
-        #         if not prefix:
-        #             return ""
-        # return prefix
+        # Start by assuming the first string is the common prefix
+        prefix = strs[0]
+        for s in strs[1:]:
+            # Adjust the prefix until it matches the beginning of each string
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
