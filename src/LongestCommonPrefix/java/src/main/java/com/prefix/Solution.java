496de3520af0ba637c35ae442613cd2f0fa75ac5
Bob Jacobs <bob.jacobs@outlook.com>
2024-04-26 19:42:43 -0400

    pass

--- src/LongestCommonPrefix/java/src/main/java/com/prefix/Solution.java
+++ src/LongestCommonPrefix/java/src/main/java/com/prefix/Solution.java
@@ -2,16 +2,15 @@ package com.prefix;
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        return "zzz";
-        // if (strs == null || strs.length == 0) return "";
+        if (strs == null || strs.length == 0) return "";
 
-        // String prefix = strs[0];
-        // for (int i = 1; i < strs.length; i++) {
-        //     while (strs[i].indexOf(prefix) != 0) {
-        //         prefix = prefix.substring(0, prefix.length() - 1);
-        //         if (prefix.isEmpty()) return "";
-        //     }
-        // }
-        // return prefix;
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
     }
 }
